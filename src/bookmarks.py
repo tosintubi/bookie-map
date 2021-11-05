2529ae00f3bfa7abf79ff4dd5e77ab9b84cb64de
Tosin <tosintubi@users.noreply.github.com>
2021-11-05 22:44:07 +0100

    factory methods

---
+++ src/bookmarks.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+bookmarks = Blueprint('bookmarks', __name__, url_prefix='/api/v1/bookmarks')
+
+
+@bookmarks.get('/')
+def get_all():
+    return []
+
+
+@bookmarks.get('/me')
+def me():
+    return {'user': 'me'}
