f4f577e29cfe50473e846f606855f7cd4fc70b1c
Tosin <tosintubi@users.noreply.github.com>
2021-12-03 23:31:59 +0100

    Heroku Postgreql hack

--- src/__init__.py
+++ src/__init__.py
@@ -15,9 +15,14 @@ def create_app(test_config=None):
     app: Flask = Flask(__name__, instance_relative_config=True)
     
     if test_config is None:
+        # Heroku Postgreql hack.
+        db_url = os.environ.get('DATABASE_URL')
+        if db_url.startswith('postgres://'):
+            db_uri = db_url.replace('postgres://', 'postgresql://')
+            
         app.config.from_mapping(
             SECRET_KEY=os.environ.get('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI'),
+            SQLALCHEMY_DATABASE_URI=db_uri,
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             JSON_SORT_KEYS=False
         )
