696916733a3e3001ee2f93c4479ccce55677f753
Tosin <tosintubi@users.noreply.github.com>
2021-12-05 22:10:14 +0100

    Resolve comments raised in the last push

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 
 from src.models import db
 from src.google import google_bp
-from src.home import home
 from src.manage import create_tables
 
 load_dotenv()
@@ -16,14 +15,18 @@ load_dotenv()
 def create_app(test_config=None):
     app: Flask = Flask(__name__, instance_relative_config=True)
     
-    if test_config is None:
+    if  not test_config:
         # Heroku Postgrseql hack.
         db_url = str(os.environ.get('DATABASE_URL'))
         if db_url.startswith('postgres://'):
             db_url = db_url.replace('postgres://', 'postgresql://',1)
         
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        if not SECRET_KEY:
+            return 'secret key is missing'
+        
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SECRET_KEY=SECRET_KEY,
             SQLALCHEMY_DATABASE_URI=db_url,
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             JSON_SORT_KEYS=False
@@ -39,7 +42,6 @@ def create_app(test_config=None):
 
     # Register blueprints
     app.register_blueprint(google_bp)
-    app.register_blueprint(home)
     
     #create table commands
     app.cli.add_command(create_tables)
--- src/manage.py
+++ src/manage.py
@@ -2,9 +2,19 @@ import click
 from flask.cli import with_appcontext
 
 from src import db
-# from src.models import  Author, Book, Borrow, UserLogin, UserProfile
 
+
+# Custom commands to manage migrations
+# https://flask.palletsprojects.com/en/2.0.x/cli/#custom-commands
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
+    db.create_all()
+
+
+# This deletes the 
+@click.command(name='drop_create_tables')
+@with_appcontext
+def drop_create():
+    db.drop_all()
     db.create_all()
